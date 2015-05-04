17d5b1d66443248a56352bd57526e7e64d9c8f1d
mmcnew <mike@visceralconcepts.com>
2015-05-04 14:27:39 -0700

    Stupid Brackets
    Missed a curly bracket that may have been destroying the whole plugin
    function.

--- vc-site-maint-add-on.php
+++ vc-site-maint-add-on.php
@@ -4,7 +4,7 @@
   Plugin Name: VC Site Maintenance Add-On
   Plugin URI: http://www.visceralconcepts.com
   Description: Generates all of the necessary functions for the Site Maintenance contract.
-  Version: 1.05.04.02
+  Version: 1.05.04.03
   Author: Visceral Concepts
   Author URI: http://www.visceralconcepts.com
   License: GPLv3 or Later
@@ -331,6 +331,8 @@ function vc_link_style() {
 			}
 		</style>';
 	}
+	
+}
 	
 	
 
