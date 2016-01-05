b43cea7251e5d7b79230a41a70ba235fc6df3716
mmcnew <mike@visceralconcepts.com>
2016-01-05 13:14:28 -0800

    Added Capability
    Gave Site Owners the ability to promote users. This was done to fix
    issues with BBPress forum roles. If there is a way to do this
    differently, it will be corrected in the future.

--- vc-site-maint-add-on.php
+++ vc-site-maint-add-on.php
@@ -4,7 +4,7 @@
   Plugin Name: VC Site Maintenance Add-On
   Plugin URI: http://www.visceralconcepts.com
   Description: Generates all of the necessary functions for the Site Maintenance contract.
-  Version: 1.07
+  Version: 1.08
   Author: Visceral Concepts
   Author URI: http://www.visceralconcepts.com
   License: GPLv3 or Later
@@ -51,6 +51,7 @@ function add_roles_on_plugin_activation() {
 			'read_private_posts' => true,
 			'read' => true,
 			'remove_users' => true,
+			'promote_users' => true,
 			'upload_files' => true,
 			'list_users' => true,
 			'remove_users' => true,
